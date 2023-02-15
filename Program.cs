8cd018415281612d457ed873b541e0358ab37111
Natalia_Go <gorshana@gmail.com>
2023-02-15 22:51:56 +0300

    1 task

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 34: Задайте массив,
+//  заполненный случайными положительными трёхзначными числами.
+//   Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int[] GenerateArray(int size)
+{
+    int[] array = new int[size];
+    var rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(100, 1000);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
+int QuontEven(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            result++;
+        }
+    }
+    return result;
+}
+
+
+
+int[] myArray = GenerateArray(10);
+PrintArray(myArray);
+System.Console.WriteLine($"количество чётных чисел в массиве равно {QuontEven(myArray)}");
+
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+
+
+// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+// // [3 7 22 2 78] -> 76
